ea96ca7536ab366e8b945b661bed38e4ad333a9e
UlugbekToshtemirovEPAM <Ulugbek_Toshtemirov@epam.com>
2024-05-24 23:21:36 +0500

    Add Modul-5 Task1 and Task2

---
+++ Modul-5/Modul5.Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Modul5.Task1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a line (type exit to quit): ");
+            while (true)
+            {
+                string line = Console.ReadLine();
+
+                if (line.ToLower().Trim() == "exit") break;
+
+                try
+                {
+                    if (string.IsNullOrEmpty(line))
+                    {
+                        throw new ArgumentException("The input line cannot be empty.");
+                    }
+
+                    Console.WriteLine(line[0]);
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
---
+++ Modul-5/Modul5.Task2/NumberParser.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul5.Task2
+{
+    public class NumberParser : INumberParser
+    {
+        public int Parse(string stringValue)
+        {
+            if (stringValue == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            stringValue = stringValue.Trim();
+
+            if (string.IsNullOrEmpty(stringValue))
+            {
+                throw new FormatException();
+            }
+
+            bool isNegative = false;
+            int i = 0;
+            if (stringValue[i] == '-')
+            {
+                isNegative = true;
+                i++;
+            }
+            else if (stringValue[i] == '+')
+            {
+                i++;
+            }
+
+            int number = 0;
+            while (i < stringValue.Length)
+            {
+                int digit = stringValue[i++] - '0';
+                if (digit < 0 || digit > 9)
+                {
+                    throw new FormatException();
+                }
+
+                if (isNegative)
+                {
+                    if (number < (int.MinValue + digit) / 10)
+                    {
+                        throw new OverflowException();
+                    }
+                    number = number * 10 - digit;
+                }
+                else
+                {
+                    if (number > (int.MaxValue - digit) / 10)
+                    {
+                        throw new OverflowException();
+                    }
+                    number = number * 10 + digit;
+                }
+            }
+
+            return number;
+        }
+    }
+}
