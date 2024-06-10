601717a71da5e0a3b95ab20461b9f8fd02e102af
UlugbekToshtemirovEPAM <Ulugbek_Toshtemirov@epam.com>
2024-06-10 07:29:16 +0500

    Modul-6.

---
+++ Modul-6/ConfigurationApp1/MySettings.cs
@@ -0,0 +1,19 @@
+﻿using ConfigurationApp1.Attributes;
+
+namespace ConfigurationApp1
+{
+    public class MySettings : ConfigurationComponentBase
+    {
+        [FileConfigurationItem("MyIntSetting")]
+        [ConfigurationManagerConfigurationItem("MyIntSetting")]
+        public int MyIntSetting { get; set; }
+
+        [FileConfigurationItem("MyFloatSetting")]
+        [ConfigurationManagerConfigurationItem("MyFloatSetting")]
+        public float MyFloatSetting { get; set; }
+
+        [FileConfigurationItem("MyStringSetting")]
+        [ConfigurationManagerConfigurationItem("MyStringSetting")]
+        public string MyStringSetting { get; set; }
+    }
+}
