75424e122c2d9c7ac56e4ea85271b900e1131144
UlugbekToshtemirovEPAM <Ulugbek_Toshtemirov@epam.com>
2024-06-28 23:54:39 +0500

    Modul-9 Task

---
+++ Modul-9/Modul9.Task1/Script.PostDeployment1.sql
@@ -0,0 +1,22 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+INSERT INTO Person (Id, FirstName, LastName)
+VALUES (1, 'Ulugbek', 'Toshtemirov');
+
+INSERT INTO Address (Id, Street, City, State, ZipCode)
+VALUES (1, '100 Main St.', 'Samarkand', 'Uzbekistan', '140100');
+
+INSERT INTO Employee (Id, AddressId, PersonId, CompanyName, Position, EmployeeName)
+VALUES (1, 1, 1, 'EPAM', 'Software Engeenare', 'Ulugbek Toshtemirov');
+
+INSERT INTO Company (Id, Name, AddressId)
+VALUES (1, 'EPAM', 1);
