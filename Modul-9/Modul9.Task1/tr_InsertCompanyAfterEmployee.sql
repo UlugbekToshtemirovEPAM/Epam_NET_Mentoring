5033d59c81fe1508539df63730dc41eb7155e99d
UlugbekToshtemirovEPAM <Ulugbek_Toshtemirov@epam.com>
2024-06-29 14:50:20 +0500

    Modul-9 added

---
+++ Modul-9/Modul9.Task1/tr_InsertCompanyAfterEmployee.sql
@@ -0,0 +1,21 @@
+﻿CREATE TRIGGER tr_InsertCompanyAfterEmployee
+ON Employee
+AFTER INSERT
+AS
+BEGIN
+    DECLARE @NewAddressId INT;
+    DECLARE @NewEmployeeId INT;
+    DECLARE @CompanyName NVARCHAR(20);
+
+    SELECT @NewEmployeeId = INSERTED.Id, @CompanyName = INSERTED.CompanyName FROM INSERTED;
+
+    -- Copy the address from the inserted employee record
+    INSERT INTO Address (Street, City, State, ZipCode)
+    SELECT Street, City, State, ZipCode FROM Address WHERE Id = (SELECT AddressId FROM Employee WHERE Id = @NewEmployeeId);
+
+    SET @NewAddressId = SCOPE_IDENTITY(); -- Get the newly inserted Address.Id
+
+    -- Insert the new company with the copied address
+    INSERT INTO Company (Name, AddressId)
+    VALUES (@CompanyName, @NewAddressId);
+END;
